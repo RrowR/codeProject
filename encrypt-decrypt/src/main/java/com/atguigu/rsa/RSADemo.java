f46429beb8622658c43351875a6ff1ba8fd82d69
Memo <2415782224@qq.com>
2020-12-22 22:38:54 +0800

    私钥加密

--- encrypt-decrypt/src/main/java/com/atguigu/rsa/RSADemo.java
+++ encrypt-decrypt/src/main/java/com/atguigu/rsa/RSADemo.java
@@ -1,7 +1,9 @@
 package com.atguigu.rsa;
 
 import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+import org.junit.Test;
 
+import javax.crypto.Cipher;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
@@ -12,6 +14,7 @@ import java.security.PublicKey;
  */
 public class RSADemo {
     public static void main(String[] args) throws Exception {
+        String input = "美美";
         //选择加密算法
         String algorithm = "RSA";
 //        KeyPairGenerator创建密钥对对象
@@ -27,8 +30,18 @@ public class RSADemo {
         //使用Base64进行转码
         String privateEncode = Base64.encode(privateKeyEncoded);
         String publicEncode = Base64.encode(publicKeyEncoded);
-        System.out.println("私钥是:"+privateEncode);
+        /*System.out.println("私钥是:"+privateEncode);
         System.out.println();
-        System.out.println("公钥是:"+publicEncode);
+        System.out.println("公钥是:"+publicEncode);*/
+
+        //创建加密对象,并传入加密算法，这里是RSA非对称加密
+        Cipher cipher = Cipher.getInstance(algorithm);
+        //传入加密规则，传入私钥加密
+        cipher.init(Cipher.ENCRYPT_MODE,privateKey);
+        byte[] bytes = cipher.doFinal(input.getBytes());
+        System.out.println(Base64.encode(bytes));
+
+
     }
+
 }
